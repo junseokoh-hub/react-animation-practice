cc93058d3040e62ee65ede40bd75bbda7365cdbd
junseokoh-hub <lunulatecat@naver.com>
2022-07-15 11:41:19 +0900

    make gestures with while-related props

--- src/App.tsx
+++ src/App.tsx
@@ -13,55 +13,32 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 12.5em;
   height: 12.5em;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 2.5em;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
-  height: 4.5em;
-  width: 4.5em;
-  border-radius: 50%;
-  place-self: center;
-  background-color: white;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   const boxVariants = {
-    start: { opacity: 0, scale: 0.5 },
-    end: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        type: "spring",
-        duration: 0.5,
-        bounce: 0.5,
-        delayChildren: 0.2,
-        staggerChildren: 0.2,
-      },
+    hover: { scale: 1.5, rotateZ: 90 },
+    click: { scale: 1, borderRadius: "50%" },
+    drag: {
+      backgroundColor: "rgba(0, 206, 201,1.0)",
+      transition: { duration: 10 },
     },
   };
 
-  const circleVariants = {
-    start: { opacity: 0, y: 10 },
-    end: {
-      opacity: 1,
-      y: 0,
-    },
-  };
   return (
     <>
       <GlobalStyle />
       <Wrapper>
-        <Box variants={boxVariants} initial="start" animate="end">
-          <Circle variants={circleVariants} />
-          <Circle variants={circleVariants} />
-          <Circle variants={circleVariants} />
-          <Circle variants={circleVariants} />
-        </Box>
+        <Box
+          drag
+          variants={boxVariants}
+          whileHover={"hover"}
+          whileTap={"click"}
+          whileDrag={"drag"}
+        />
       </Wrapper>
     </>
   );
