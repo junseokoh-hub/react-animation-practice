8d858759d4edac3765e383fb89af142750c00d48
junseokoh-hub <lunulatecat@naver.com>
2022-07-15 15:17:38 +0900

    make slider with next btn

--- src/App.tsx
+++ src/App.tsx
@@ -12,38 +12,53 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-  width: 12.5em;
+  width: 26.5em;
   height: 12.5em;
-  background-color: rgba(255, 255, 255, 1);
   border-radius: 2.5em;
+  position: absolute;
+  top: 6em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 1);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: { opacity: 0, scale: 0 },
-  visible: { opacity: 1, scale: 1, rotateZ: 360 },
-  leaving: { opacity: 0, scale: 0, y: 50 },
+const box = {
+  invisible: { x: 500, opacity: 0, scale: 0 },
+  visible: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
+  exit: {
+    x: -500,
+    opacity: 0,
+    scale: 0,
+    rotateX: 180,
+    transition: { duration: 1 },
+  },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
-
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
   return (
     <>
       <GlobalStyle />
       <Wrapper>
-        <button onClick={toggleShowing}>Click Me!</button>
         <AnimatePresence>
-          {showing ? (
-            <Box
-              variants={boxVariants}
-              initial="initial"
-              animate="visible"
-              exit="leaving"
-            />
-          ) : null}
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+            i === visible ? (
+              <Box
+                variants={box}
+                initial="invisible"
+                animate="visible"
+                exit="exit"
+                key={i}
+              >
+                {i}
+              </Box>
+            ) : null,
+          )}
         </AnimatePresence>
+        <button onClick={nextPlease}>Next</button>
       </Wrapper>
     </>
   );
